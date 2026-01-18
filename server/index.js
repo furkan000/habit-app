bd74dff0d744eafc02688af74bbfa027ecdff359
furkan <f.tas@bilduin.de>
2026-01-18 09:26:05 +0100

    listen on 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -283,7 +283,8 @@ app.put('/api/logs/:id', requireTenant, async (req, res) => {
 });
 
 const PORT = 3001;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
